665ec56704744d1f68047de792407b60605b67fc
Galo Naranjo <gnaranjo411@gmail.com>
2024-09-21 06:16:12 -0400

    Joblistings page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import AddJobsPage from "./pages/AddJobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
       <Route path="/add-job" element={<AddJobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
